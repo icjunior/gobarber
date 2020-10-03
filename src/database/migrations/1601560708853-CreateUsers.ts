c39a74d0d60b541015fa61bf76bfddc92fb557f5
Ismael Junior <icjunior@bigsupermercados.com.br>
2020-10-03 01:43:50 -0300

    Alterações

--- src/database/migrations/1601560708853-CreateUsers.ts
+++ src/database/migrations/1601560708853-CreateUsers.ts
@@ -1,12 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
-import { uuid } from 'uuidv4';
 
-export default class CreateAppointments1601327376073
-    implements MigrationInterface {
+export default class CreateUsers1601560708853 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
@@ -16,14 +14,17 @@ export default class CreateAppointments1601327376073
                         default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
-                        isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false,
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'create_at',
@@ -41,6 +42,6 @@ export default class CreateAppointments1601327376073
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users');
     }
 }
